58ca4f65d5ded625dbbef9b3e0f64c77ab1a109a
Carlos Gomez <carlosandres0741@gmail.com>
2024-02-02 22:34:21 -0500

    fix: changed openai by gemini

--- src/App.jsx
+++ src/App.jsx
@@ -6,48 +6,42 @@ const App = () => {
   const [loading, setLoading] = React.useState(false)
   const [text, setText] = React.useState('')
   const [translation, setTranslation] = React.useState('Aquí aparecerá la traducción')
-  const {VITE_OPENAI_API_KEY} = import.meta.env
+  const { VITE_GEMINI_API_KEY } = import.meta.env
 
   const getTranslation = async event => {
     event.preventDefault()
 
     setLoading(true)
 
-    const response = await fetch('https://api.openai.com/v1/completions', {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${VITE_GEMINI_API_KEY}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${VITE_OPENAI_API_KEY}`
       },
       body: JSON.stringify({
-        model: 'text-davinci-003',
-        prompt: `Translate this into Spanish: ${text}`,
-        temperature: 0.3,
-        max_tokens: 600,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
+        contents: [{
+          parts: [{
+            text: `Translate this text to spanish: ${text}`
+          }]
+        }]
       })
     })
 
     const data = await response.json()
 
     if (text) {
-      setTranslation(data.choices[0].text)
-    } else {
-      setTranslation(data.choices[0].text.split("\n")[4])
-      setText(data.choices[0].text.split("\n")[2])
+      setTranslation(data.candidates[0].content.parts[0].text)
     }
 
     setLoading(false)
   }
 
   return (
-    <main className='flex gap-8 items-center w-full flex-col text-cyan-100 p-8'>
+    <main className='container flex gap-8 items-center w-full flex-col text-cyan-100 p-8 m-auto'>
       <h1 className='text-3xl text-center'>Traductor con inteligencia artificial</h1>
-      <form 
+      <form
         onSubmit={getTranslation}
-        className='flex gap-16 flex-wrap justify-center items-center'
+        className='flex gap-16 flex-col flex-wrap justify-center items-center xl:flex-row'
       >
         <textarea
           className='w-96 h-96 bg-slate-800 resize-none outline-none p-4 rounded-xl'
@@ -61,12 +55,12 @@ const App = () => {
         >
           Traducir
           <ChangeIcon />
-          </button>
-        
-          {loading 
-            ? <p className='w-96 h-96 flex justify-center items-center'><LoadingIcon/></p> 
-            : <p className='w-96 h-96 flex justify-center overflow-scroll p-4'>{translation}</p>
-          }
+        </button>
+
+        {loading
+          ? <p className='w-96 h-96 flex justify-center items-center'><LoadingIcon /></p>
+          : <p className='w-96 h-96 flex justify-center overflow-y-scroll p-4'>{translation}</p>
+        }
       </form>
     </main>
   )
